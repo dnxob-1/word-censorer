0db2572fe06b5e0700495681bff170af56d0cbd1
morgan <ff6.6.ffdnxob.pthd.6@gmail.com>
2025-02-06 13:16:50 -0600

    added files

---
+++ main.py
@@ -0,0 +1,24 @@
+import re
+
+## not the most advanced word censorer. includes profanities. beware.  
+
+listOfBadWords = ["fuck", "shit", "bitch", "slut", "harlot", "nigga", "nigger", "whore", "cunt", "ass", "fucker", "faggot", "cock", "dick", "chink", "hoe", "cuck", "motherfucker", "pussy", "dumbass", "porn", "butthole", "jackass", "moron", "dumbass", "dumb", "fucking", "tittes", "tits", "cocksucker", "retard"]
+
+
+def userIn():
+    strInput = str(input("Enter sentence: "))
+    return strInput.lower()
+
+
+def wordCensorer():
+    getInput = userIn() 
+    getInput = re.findall(r'[A-Za-z]+|\d+', getInput)
+
+    for i in getInput:
+        for x in listOfBadWords:
+            if i == x:
+                lengthOfCensoring = "#" * len(x)
+                i = i.replace(i, lengthOfCensoring)
+        print(i)
+
+wordCensorer()
